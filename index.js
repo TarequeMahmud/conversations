a058d050699dd09d7fde6cd99e0398007b4068a8
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-12-18 08:56:28 +0600

    feat: emit events for message

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
   socket.on("disconnect", () => {
     console.log("a user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
   });
 });
 
